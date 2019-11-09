f437d2212eb01f53daf75f5100cb5f549b7d214d
maks <maksimlya@gmail.com>
2019-11-09 11:55:27 +0200

    Performance optimization

--- src/start.js
+++ src/start.js
@@ -1,65 +1,44 @@
 const electron = require('electron')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
-const path = require('path');
-const webpack = require('webpack');
-const WebpackDevServer = require('webpack-dev-server');
+const port = 1111;
+const perf = require('execution-time')();
 const config = require('../webpack.config');
-const port = 1212;
-
-const options = {
-  publicPath: '/',
-  hot: false,
-  liveReload:false,
-  inline: true,
-  contentBase: path.join(__dirname, 'dist'),
-  stats: {
-    colors: true,
-    hash: false,
-    version: false,
-    timings: false,
-    assets: false,
-    chunks: false,
-    modules: false,
-    reasons: false,
-    children: false,
-    source: false,
-    errors: true,
-    errorDetails: true,
-    warnings: true,
-    publicPath: true
-  }
-};
-
-let devServer = new WebpackDevServer(webpack(config),options);
-
-devServer.listen(port,'localhost', function(err){
-  if(err) {
-    console.error(err);
-  }
 
 
-  app.on('ready', createWindow)
+perf.start('rend');
 
-  app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-      app.quit()
-    }
-  })
-  
-  app.on('activate', () => {
-    if (mainWindow === null) {
-      createWindow()
-    }
-  })
-})
-
+// const options = {
+//   publicPath: '/',
+//   hot: false,
+//   liveReload:false,
+//   inline: true,
+//   contentBase: path.join(__dirname, 'dist'),
+//   stats: {
+//     colors: true,
+//     hash: false,
+//     version: false,
+//     timings: false,
+//     assets: false,
+//     chunks: false,
+//     modules: false,
+//     reasons: false,
+//     children: false,
+//     source: false,
+//     errors: true,
+//     errorDetails: true,
+//     warnings: true,
+//     publicPath: true
+//   }
+// };
 
 let mainWindow
 
 
 
 function createWindow() {
+
+
 
   mainWindow = new BrowserWindow({
        width: 800,
@@ -72,11 +51,27 @@ function createWindow() {
      
   mainWindow.webContents.openDevTools()
 
-  mainWindow.loadURL('http://localhost:' + port);
+  mainWindow.loadURL('http://localhost:' + port).then(() => {
+    let res = perf.stop('rend');
+
+    console.log('Rend process:',Math.round(res.time/10)/100);
+  })
 
   mainWindow.on('closed', () => {
     mainWindow = null
   })
-
 }
 
+app.on('ready', createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,22 @@
+
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const path = require('path');
+const port = 1111;
+const publicPath = `http://localhost:${port}`;
+const child_process = require('child_process');
+const perf = require('execution-time')();
 
+perf.start('webpack');
 
 module.exports = {
+	
 	mode: 'development',
 	devtool: 'source-map',
+	
+	entry: [
+		`webpack-dev-server/client?http://localhost:${port}/`,
+		require.resolve('./src/index')
+	],
 	module: {
 		rules: [
 		{
@@ -54,7 +66,42 @@ module.exports = {
 		
 	  ],
 	  output: {
+		publicPath: `http://localhost:${port}/`,
+		filename: 'renderer.dev.js',
 		devtoolModuleFilenameTemplate: info => path.resolve(info.absoluteResourcePath).replace(/\\/g, '/'),
+	  },
+
+	  devServer: {
+		port,
+		publicPath,
+		compress: false,
+		noInfo: true,
+		stats: 'errors-only',
+		inline: true,
+		lazy: false,
+		hot: true,
+		headers: { 'Access-Control-Allow-Origin': '*' },
+		contentBase: path.join(__dirname, 'dist'),
+		watchOptions: {
+		  aggregateTimeout: 300,
+		  ignored: /node_modules/,
+		  poll: 100
+		},
+		historyApiFallback: {
+		  verbose: true,
+		  disableDotRule: false
+		},
+		before() {
+			console.log('Starting Main Process...');
+			child_process.spawn('npm', ['run', 'start-main-dev'], {
+			  shell: true,
+			  env: process.env,
+			  stdio: 'inherit'
+			})
+			  .on('close', code => process.exit(code))
+			  .on('error', spawnError => console.error(spawnError));
+		  
+		}
 	  }
 	 
 };
