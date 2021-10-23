1cb26ce23085e63a78ad17cac32b926422f6e55f
Maksim <maksimlya@gmail.com>
2021-10-23 03:32:02 +0300

    Updated to latest versions
    - Electron 15
    - Webpack 5
    - Prod config

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-
-window.ELECTRON_DISABLE_SECURITY_WARNINGS = true;
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
